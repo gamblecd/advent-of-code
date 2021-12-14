6026734997a80c1b39fcf71a4ba7b878be24a079
Chris Gamble <shadowstorm50@gmail.com>
2021-12-13 17:07:30 -0800

    day13 solved

--- Day13/answer.py
+++ Day13/answer.py
@@ -1,5 +1,6 @@
 import fileinput, bisect
 import functools
+from collections import defaultdict
 import os
 import sys
 
@@ -8,12 +9,57 @@ mymodule_dir = os.path.join( script_dir, '..', 'utils' )
 sys.path.append( mymodule_dir )
 
 from util import timer_func as timer
-filename = "Day13/inputs/ex.txt"
-
+filename = "Day13/inputs/actual.txt"
+grid = defaultdict(lambda: '.')
+instructions = []
+empty = False
+xmax = 0
+ymax = 0
 for line in fileinput.input(files=(filename)):
     line_data = line.strip()
+    if line_data == "":
+        empty = True
+        continue
+    if not empty:
+        x,y =  (int(x)for x in line_data.split(","))
+        xmax = max(x, xmax)
+        ymax = max(y, ymax)
+        grid[(x,y)] = "#"
+    else:
+        instructions.append(line_data.split(" ")[2])
+        
 
-    
-print("Part 1: ",)
+def print_mapped_grid(grid, xmax,ymax):
+    for y in range(ymax+1):
+        line = ""
+        for x in range(xmax+1):
+            if (x,y) in grid.keys():
+                line += grid[(x,y)]
+            else:
+                line += "."
+        print(line)            
+def fold(grid, command, xmax, ymax):
+    direction, line = command.split("=")
+    line = int(line)
+    if (direction == 'y'):
+        for (x,y) in list(grid.keys()):
+            if (y >= line):
+                ynew = line - (y-line)
+                dot = grid[(x,y)]
+                grid[(x,ynew)] = dot
+                grid.pop((x,y))
+        return (xmax, line-1)
+    if (direction == 'x'):
+        for (x,y) in list(grid.keys()):
+            if (x >= line):
+                xnew = line - (x-line)
+                dot = grid[(x,y)]
+                grid[(xnew,y)] = dot
+                grid.pop((x,y))
+        return (line-1, ymax)
+for instr in instructions:
+    xmax,ymax = fold(grid, instr, xmax, ymax)
+print("Part 1: ",len(grid.keys()))
 print()
-print("Part 2: ",)
+print("Part 2: ",print_mapped_grid(grid, xmax, ymax)
+)
