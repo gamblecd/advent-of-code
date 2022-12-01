d9d7ff8eca7bd782a4b9bacbdd489c68b57c4ad2
Chris Gamble <cgamble@salesforce.com>
2022-12-01 13:11:48 -0800

    adding readme and day 2 for 2022

---
+++ 2022/day2/answer.py
@@ -0,0 +1,19 @@
+import fileinput, bisect
+import functools
+import os
+import sys
+
+script_dir = os.path.dirname( __file__ )
+mymodule_dir = os.path.join( script_dir, '..', 'utils' )
+sys.path.append( mymodule_dir )
+
+from util import timer_func as timer
+filename = os.path.join(script_dir, "inputs", "file.txt");
+
+for line in fileinput.input(files=(filename)):
+    line_data = line.strip()
+
+    
+print("Part 1: ",)
+print()
+print("Part 2: ",)
