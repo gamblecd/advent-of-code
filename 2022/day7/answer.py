682d787d08df4a0646f00b6d0b0807ee1712d701
Chris Gamble <cgamble@salesforce.com>
2022-12-07 15:41:26 -0800

    day 7

--- 2022/day7/answer.py
+++ 2022/day7/answer.py
@@ -15,11 +15,82 @@ if (run_actual):
 else:
     filename = os.path.join(script_dir, "inputs", "ex.txt")
 
+dirs = []
+
+root = {"name":"/", "parent": None, "dirs":{}, "files": {}}
+
+current = root;
+
+total_space = 70000000;
+target_free_space = 30000000;
+def parseCommand(line):
+    cmdParts = line.split()
+    cmd = cmdParts[1]
+    location = cmdParts[2]
+    return (cmd, location)
+
+def gatherSize(dir):
+    size = 0
+    for f in dir['files'].values():
+        size += f['size']
+       
+    for d in dir['dirs'].values():
+        if 'size' not in d.keys():
+            gatherSize(d)
+        size += d['size']
+    dir['size'] = size    
+    dirs.append(dir)
+    return size
+
+def executeCD(loc):
+    global current;
+    if loc == "..":
+        # before we go up lets see if we can get the size of current.
+        # Optimization
+        #gatherSize(current)
+        
+        current = current['parent']
+    elif loc == "/":
+        current = root;
+    else:
+        current = current['dirs'][loc]
+        
 
 for line in fileinput.input(files=(filename)):
     line_data = line.strip()
+    if line_data[0] == '$':
+        cmd = line_data.split();
+        if len(cmd) == 3:
+            loc = cmd[2]
+            executeCD(loc)
+    else:
+        output, name = line_data.split()
+        if output == "dir":
+            current['dirs'][name] = {'name': name, "parent": current, "dirs": {}, "files": {}}
+        else:
+            current['files'][name] = {"name": name, "size": int(output)}
 
-    
-print("Part 1: ",)
+@timer
+def gatherRoot():
+    global root;
+    gatherSize(root)
+
+gatherRoot();
+
+dirs.sort(key=lambda x: x['size'], reverse=True)
+dirs.pop(0)
+
+t = 0
+
+actual_space = total_space - root['size']
+goal = target_free_space - actual_space
+target = None
+for i, d in enumerate(dirs):
+    size = d['size']
+    if size <= 100000:
+        t += size
+    if not target and size <= goal:
+        target = dirs[i-1]
+print("Part 1: ", t)
 print()
-print("Part 2: ",)
+print("Part 2: ",target['size'])
