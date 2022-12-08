b07d4389d62b21870441f783ad69bd5bec84799f
Chris Gamble <cgamble@salesforce.com>
2022-12-08 13:46:22 -0800

    speeding up and cleaning up day8

--- 2022/day8/answer.py
+++ 2022/day8/answer.py
@@ -1,4 +1,4 @@
-import fileinput, bisect
+import fileinput
 import functools
 import operator
 import os
@@ -29,38 +29,31 @@ def get_tree(tree):
     global tree_grid;
     return tree_grid[tree[1]][tree[0]] # y,x
 
-
-def is_taller(tree1, tree2):
-    global tree_grid
-    return get_tree(tree1) > get_tree(tree2)
-
 def is_edge(tree):
     x, y = tree
     if x in row_edges or y in column_edges:
         return True
 
-def is_outside(potential_fake_tree):
-    x, y = potential_fake_tree
-    if (x < row_edges[0] or x > row_edges[1]) or (y < row_edges[0] or y > row_edges[1]):
+def is_tallest(tree, direction):
+    if (len(direction) == 0):
         return True
+    for t in direction:
+        if t >= get_tree(tree):
+            return False
+    return True
 
 def is_visible(tree):
     x,y = tree
     if is_edge(tree):
-        return True
+        return (True, 0)
     else:
-        tree_directions = [(-1,0), (1,0), (0,-1), (0,1)]
-        for dir in tree_directions:
-            new_tree = tree
-            tallest = True
-            # For Each direction continue until we hit an edge
-            while tallest and not is_edge(new_tree):
-                new_tree = (new_tree[0]+dir[0], new_tree[1] + dir[1])
-                tallest = is_taller(tree, new_tree)
-            # Find if tallest tree in row
-            if tallest:
-                return True
-        return False;
+        directions = get_directions(tree)
+        for dir in directions: 
+            if is_tallest(tree, dir):
+                scenic_score = scenic(tree, directions)
+                return (True, scenic_score)
+        return (False, 0)
+
 
 def count_until_blocked(tree_height, direction):
     if (len(direction) == 0):
@@ -72,54 +65,36 @@ def count_until_blocked(tree_height, direction):
             return count
     return count
 
-
-def increment_tree(tree, dir):
-    return (tree[0]+dir[0], tree[1] + dir[1])
-
-def scenic(tree):
-    global column_edges;
+def get_directions(tree):
     global tree_grid
-    first_tree = tree
-    if is_visible(tree) and not is_edge(tree):
-        scenic_scores = []
-        tree_directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        for x,y in tree_directions:
-            directionArr = []
-            if x == 1:
-                directionArr  = tree_grid[tree[1]][tree[0]+1:]
-            elif x == -1:
-                directionArr = tree_grid[tree[1]][0:tree[0]]
-                directionArr = [z for z in reversed(directionArr)]
-            elif y == 1:
-                for i in range(tree[1]+1, column_edges[1]+1):
-                    directionArr.append(tree_grid[i][tree[0]])
+    return [ 
+            tree_grid[tree[1]][tree[0]+1:], # -x
+            [z for z in reversed(tree_grid[tree[1]][0:tree[0]])], # +x
+            [tree_grid[i][tree[0]] for i in range(tree[1]+1, column_edges[1]+1)], # -y
+            [z for z in reversed([tree_grid[i][tree[0]] for i in range(tree[1])])] # +y
+     ]
 
-            else: 
-                for i in range(tree[1]):
-                    directionArr.append(tree_grid[i][tree[0]])
-                directionArr = [z for z in reversed(directionArr)]
-            scenic_scores.append(count_until_blocked(get_tree(tree), directionArr))
-        return functools.reduce(operator.mul, scenic_scores, 1)
-    return 0
+def scenic(tree, directions = None):
+    if not directions:
+        directions = get_directions(tree)
+    scenic_scores = []
+    for directionArr in directions:
+        scenic_scores.append(count_until_blocked(get_tree(tree), directionArr))
+    return functools.reduce(operator.mul, scenic_scores, 1)
 
 @timer
-def part1(tree_grid):
+def exec(tree_grid):
     count = 0
+    mostScenic = 0;
     for i, row in enumerate(tree_grid):
         for j, height in enumerate(row):
-            if is_visible((j, i)):
+            visible, scenic = is_visible((j, i))
+            if visible:
+                mostScenic = max(mostScenic, scenic)
                 count += 1
-    return count
+    return count, mostScenic
 
-@timer
-def part2(tree_grid):
-    best_score = 0
-    for i, row in enumerate(tree_grid):
-        for j, height in enumerate(row):
-            scenic_score = scenic((j, i))
-            best_score = max(best_score, scenic_score)
-    return best_score
-
-print("Part 1: ", part1(tree_grid))
+part1, part2 = exec(tree_grid);
+print("Part 1: ", part1)
 print()
-print("Part 2: ", part2(tree_grid))
+print("Part 2: ", part2)
