84ae1582425b89691d068cc4d590482837b31dc9
Chris Gamble <cgamble@salesforce.com>
2022-12-14 09:51:27 -0800

    day14, faster, but unsound

--- 2022/day14/answer.py
+++ 2022/day14/answer.py
@@ -12,11 +12,14 @@ from util import get_input_file, timer_func as timer
 filename = get_input_file(sys.argv[1:], script_dir)
 
 rock_paths = []
+edges = []
 for line in fileinput.input(files=(filename)):
     line_data = line.strip()
     points = [[int(p) for p in point.strip().split(",")]
               for point in line_data.split("->")]
     rock_paths.append(points)
+    edges.append(points[0])
+    edges.append(points[-1])
 
 rocks = set()
 sand = set()
@@ -124,10 +127,63 @@ def fall_grain_floor(starting):
             new_p = starting
             if old_p == starting:
                 break;
+            
+            
+# TODO dynamic program this to fill up from the bottom. 
+# find all the edges that fall in the area of sand
+# add those spots to a set
+# move upward to add subsequent edges until all edges that have been contained are managed
+# find set.
+def find_sand(point, floor):
+    points = set()
+    x, y = point
+    spread = 0
+    height = 0
+    points.add(point)
+    while height != floor:
+        for i in range(x-spread, x+spread+1):
+            points.add((i, y+height))
+        height += 1
+        spread += 1
+    return points
 
 
+def all_spaces():
+    all_spaces = 0
+    for path in rock_paths:
+        for i, point in enumerate(path):
+            x = point[0]
+            y = point[1]
+            if i != len(path)-1:  # If we aren't at the end connect the dots
+                next_point = path[i+1]
+                all_spaces += find_space_count(point,next_point, floor)
+    return all_spaces
+
+
+def find_space_count(point1,point2, floor):
+    points = set()
+    if (point1[0] == point2[0]):
+        return 0
+    x_distance = abs(point1[0]-point2[0])+1
+    if (x_distance < 3):
+        return 0;
+    y = point1[0]
+    height = 1
+    spaces = 0
+    while y+height != floor-1 and x_distance > 1:
+        x_distance = x_distance - 2*height
+        spaces += x_distance
+        height+=1
+        
+    return spaces
+
+
+all_possible_points = len(find_sand((500, 0), floor))
+print(all_possible_points - all_spaces() - len(rocks))
+#all possible - rocks - each x path inverted
+# assumes there's no cavities thats a failure
 fall_grain((500, 0))
-print_outcome(rocks,sand,(500,0))
+#print_outcome(rocks,sand,(500,0))
 print("Part 1: ",len(sand))
 sand = set()
 falling_path = set()
