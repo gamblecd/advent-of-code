203a33d64b3156465b19877340b7f2a8fcfe9e6d
Chris Gamble <shadowstorm50@gmail.com>
2023-12-03 21:23:45 -0800

    day4 done, day5 ready

--- 2023/day4/answer.py
+++ 2023/day4/answer.py
@@ -11,22 +11,47 @@ from util import get_input_file, timer_func as timer
 filename = get_input_file(sys.argv[1:], script_dir)
 
 def prep():
+    cards = []
     lines = []
     for line in fileinput.input(files=(filename)):
         line_data = line.strip()
         lines.append(line_data)
-    return lines
+        winning_numbers, num_have  = line_data.split(":")[1].strip().split(" | ")
+        num_have = list(map(int, num_have.split()))
+        winning_numbers = list(map(int, winning_numbers.split()))
+        cards.append((winning_numbers, num_have));
+    return cards
 
+def score(count):
+    return 2**(count-1)
+
+def num_of_matches(winning_numbers, num_have):
+    sc = 0
+    for n in num_have:
+        if n in winning_numbers:
+            sc += 1;
+    return sc
 def part1(lines):
-    for line in lines:
-        pass
-    print("Part 1: ",)
+    count = 0
+    for winning_numbers, num_have in lines:
+        sc = num_of_matches(winning_numbers, num_have)
+        if (sc > 0):
+            count += score(sc);
+    print("Part 1: ",count)
 
 def part2(lines):
-    for line in lines:
-        pass
-    
-    print("Part 2: ",)
+    copies = [1 for _ in range(len(lines))]
+    curr = 0
+    for winning_numbers, num_have in lines:
+        sc = num_of_matches(winning_numbers, num_have)
+        index = 1
+        while sc > 0:
+            copies[curr+index] += copies[curr]
+            sc-=1
+            index+=1
+        curr+=1
+        
+    print("Part 2: ",sum(copies))
 
 part1(prep());
 print()
