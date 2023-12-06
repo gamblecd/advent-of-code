9574aa7e2a3355110f3aec6a0001285f92f2d0db
Chris Gamble <cgamble@salesforce.com>
2023-12-05 21:29:32 -0800

    day6 done, day 7 ready

--- 2023/day6/answer.py
+++ 2023/day6/answer.py
@@ -11,22 +11,44 @@ from util import get_input_file, timer_func as timer
 filename = get_input_file(sys.argv[1:], script_dir)
 
 def prep():
-    lines = []
-    for line in fileinput.input(files=(filename)):
-        line_data = line.strip()
-        lines.append(line_data)
-    return lines
+    time = []
+    distance = []
+    lines = [line.strip() for line in fileinput.input(files=(filename))]
+    time = list(map(int, lines[0].split()[1:]))
+    distance = list(map(int, lines[1].split()[1:]))
+    return list(zip(time, distance))
 
-def part1(lines):
-    for line in lines:
-        pass
-    print("Part 1: ",)
+# x(t-x) > distance
+# x(t-x)-distance > 0
+# -x^2 + tx -distance > 0
+# t-x > (distance / x) 
+#t > (distance / x)+x
 
-def part2(lines):
-    for line in lines:
-        pass
+def count_winners(time, distance):
+    count = 0;
+    for i in range(time):
+        if i*(time-i) > distance:
+            count +=1
+    return count
+
+def part1(races):
+    counts = []
+    for race in races:
+        time, distance = race
+        counts.append(count_winners(time, distance))
+    print("Part 1: ",functools.reduce(lambda a,b: a*b, counts, 1))
+
+def part2(races):
+    time = []
+    distance =[]
+    for race in races:
+        time.append(race[0])
+        distance.append(race[1])
+    time = int(functools.reduce(lambda a, b: a+b, [str(t) for t in time], ''))
+    distance = int(functools.reduce(lambda a, b: a+b, [str(d) for d in distance], ''))
+    print(time, distance)
     
-    print("Part 2: ",)
+    print("Part 2: ",count_winners(time, distance))
 
 part1(prep());
 print()
