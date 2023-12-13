3f16fda7d78f6fd4f0a5937007ad51d9322abbca
Chris Gamble <cgamble@salesforce.com>
2023-12-13 14:04:45 -0800

    finding mirror

--- 2023/day13/answer.py
+++ 2023/day13/answer.py
@@ -35,11 +35,13 @@ def getRow(grid, row):
     return grid[row]
 
 def compare(row1, row2):
+    incorrect = []
     for i in range(len(row1)):
         if row1[i] != row2[i]:
-            return False
-    
-    return True
+            incorrect.append(i)
+    return incorrect
+
+
 
 def findMirror(grid, column, finder=getColumn):
     gridLength = len(grid[0]) if finder == getColumn else len(grid)
@@ -51,7 +53,7 @@ def findMirror(grid, column, finder=getColumn):
     while left_index >= 0 and right_index < gridLength:
         left = finder(grid, left_index)
         right = finder(grid, right_index)
-        if not compare(left, right):
+        if  len(compare(left, right))!= 0:
             isMirror = False
             break
         else:
@@ -63,25 +65,106 @@ def findMirror(grid, column, finder=getColumn):
     else:
         return findMirror(grid, column+1, finder)
 
+def findMirror2(grid, column, finder=getColumn, canSwap = True):
+    isColumn = finder == getColumn
+    gridLength = len(grid[0]) if isColumn else len(grid)
+    if (column >= gridLength):
+        return -1
+    left_index = column
+    right_index =column+1
+    isMirror = False;
+    while left_index >= 0 and right_index < gridLength:
+        left = finder(grid, left_index)
+        right = finder(grid, right_index)
+        wrongs = compare(left, right)
+        if  len(wrongs) == 1 and canSwap:
+            ind = wrongs[0]
+            if (isColumn):
+                point1 = (left_index, ind)
+                point2 = (right_index, ind)
+            else:
+                point1 = (ind, left_index)
+                point2 = (ind,right_index)
+            lGrid = [[j for j in i] for i in grid]
+            lGrid[point1[1]][point1[0]] = right[ind]
+            rGrid = [[j for j in i] for i in grid]
+            rGrid[point2[1]][point2[0]] = left[ind]
+
+            lMirror = findMirror2(lGrid, column, finder, False);
+            if lMirror >=0:
+                print(point1, column, isColumn)
+                insertAndPrint(lGrid, column, isColumn)
+                return lMirror;
+            rMirror = findMirror2(rGrid, column, finder, False);
+            if rMirror >=0:
+                print(point2, column, isColumn)
+                insertAndPrint(rGrid, column, isColumn)
+                return rMirror;
+
+            isMirror = False;
+            break
+        elif len(wrongs) != 0:
+            isMirror = False
+            break
+        else:
+            isMirror = not canSwap
+        left_index -= 1
+        right_index += 1
+    if isMirror:
+        return column + 1
+    else:
+        return findMirror2(grid, column+1, finder, canSwap)
+    
+def insertAndPrint(grid, column, isColumn):
+    if isColumn:
+        insertColumn(grid, column+1)
+    else:
+        insertRow(grid, column+1)
+    for r in grid:
+        #print("".join(r))
+        pass
+def insertColumn(grid, column):
+    for row in grid:
+        row.insert(column, '|')
+
+def insertRow(grid, index):
+    rowLength = len(grid[0])
+    row = ['-' for _ in range(rowLength)]
+    grid.insert(index, row)
+        
+
 def getGridValue(grid):
     v_mirror = findMirror(grid, 0, getColumn)
     if (v_mirror) < 0:
         h_mirror = findMirror(grid, 0, getRow)
-        if h_mirror >-1:
+        if h_mirror > -1:
             return h_mirror * 100
     else:
         return v_mirror
+    
+def getGridValue2(grid):
+    print()
+    v_mirror = findMirror2(grid, 0, getColumn)
+    h_mirror = findMirror2(grid, 0, getRow)
+    if (v_mirror) < 0:
+        if h_mirror > -1:
+            return h_mirror * 100
+        else:
+            print('Edge')
+    else:
+        return v_mirror
+    
 def part1(grids):
     count = 0
     for g in grids:
         count += getGridValue(g)
     print("Part 1: ",count)
 
-def part2(lines):
-    for line in lines:
-        pass
-    
-    print("Part 2: ",)
+def part2(grids):
+    count = 0
+    for g in grids:
+        count += getGridValue2(g)
+    print("Part 2: ", count)
 
 part1(prep())
 print()
